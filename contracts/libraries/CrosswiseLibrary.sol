420f515bbe68c372717ab11d27b3c7451989cab1
versedpro <ultraj0330@gmail.com>
2021-07-23 03:20:48 +0800

    fix: int code hash in crosswise library

--- contracts/libraries/CrosswiseLibrary.sol
+++ contracts/libraries/CrosswiseLibrary.sol
@@ -21,7 +21,7 @@ library CrosswiseLibrary {
                 hex'ff',
                 factory,
                 keccak256(abi.encodePacked(token0, token1)),
-                hex'd0d4c4cd0848c93cb4fd1f498d7013ee6bfb25783ea21593d5834f5d250ece66' // init code hash
+                hex'3965823386146cd80eb41176d6f9bb8c2c4d5c8d95f91f985cc8051b4b68383b' // init code hash
             ))));
     }
 
